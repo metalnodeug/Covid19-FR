b2e40acf900b3779260c856811cdc0f4ad5c2c19
metalnodeug <metalnodeug@gmail.com>
2020-08-16 10:32:38 +0200

    Refactor all pickerView for the main screen

--- Covid19-FR/Screens/MainVC.swift
+++ Covid19-FR/Screens/MainVC.swift
@@ -12,6 +12,7 @@ class MainVC: UIViewController {
 
     let logoImageView = UIImageView()
     let textLabel = UILabel()
+    let entryTextField = UITextField()
     let entryPickerView = UIPickerView()
     let actionButton = CovButton()
 
@@ -33,7 +34,8 @@ class MainVC: UIViewController {
     private func configureUI() {
         configure_logoImageView()
         configure_textLabel()
-        configure_entryPickerView()
+        configure_entryTextField()
+        //        configure_entryPickerView()
         configure_actionButton()
     }
 
@@ -44,9 +46,9 @@ class MainVC: UIViewController {
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logoImageView.heightAnchor.constraint(equalToConstant: 170),
+            logoImageView.heightAnchor.constraint(equalToConstant: 190),
             logoImageView.widthAnchor.constraint(equalTo: logoImageView.heightAnchor)
         ])
     }
@@ -54,33 +56,59 @@ class MainVC: UIViewController {
     private func configure_textLabel() {
         view.addSubview(textLabel)
         textLabel.translatesAutoresizingMaskIntoConstraints = false
-        textLabel.text = "Veuillez selectionner un département:"
-        textLabel.numberOfLines = 0
+        textLabel.textAlignment = .center
+        textLabel.text = "Selectionner un département:"
+        textLabel.numberOfLines = 2
         textLabel.font = UIFont.boldSystemFont(ofSize: 17)
 
         NSLayoutConstraint.activate([
             textLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 20),
             textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            textLabel.heightAnchor.constraint(equalToConstant: 41),
+            textLabel.heightAnchor.constraint(equalToConstant: 20),
             textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
         ])
     }
 
-    private func configure_entryPickerView() {
-        view.addSubview(entryPickerView)
-        entryPickerView.translatesAutoresizingMaskIntoConstraints = false
+    private func configure_entryTextField() {
+        view.addSubview(entryTextField)
+        entryTextField.translatesAutoresizingMaskIntoConstraints = false
+        entryTextField.text = "67 - Bas-Rhin"
+        entryTextField.backgroundColor = .secondarySystemBackground
+        entryTextField.textAlignment = .center
+        entryTextField.layer.borderWidth = 2
+        entryTextField.layer.borderColor = UIColor.systemBackground.cgColor
+        entryTextField.layer.cornerRadius = 10
+        entryTextField.inputView = entryPickerView
+        createToolBar()
+
         entryPickerView.delegate = self
         entryPickerView.dataSource = self
         entryPickerView.selectRow(67, inComponent: 0, animated: false)
 
         NSLayoutConstraint.activate([
-            entryPickerView.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 10),
-            entryPickerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            entryPickerView.heightAnchor.constraint(equalToConstant: 150),
-            entryPickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            entryPickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            entryTextField.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 15),
+            entryTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            entryTextField.heightAnchor.constraint(equalToConstant: 50),
+            entryTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            entryTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
         ])
+    }
+
+    private func createToolBar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Valider", style: .plain, target: self, action: #selector(dismissPickerView))
+
+        toolbar.setItems([doneButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+
+        entryTextField.inputAccessoryView = toolbar
+    }
+
+    @objc private func dismissPickerView() {
+        view.endEditing(true)
     }
 
     private func configure_actionButton() {
@@ -91,7 +119,7 @@ class MainVC: UIViewController {
         actionButton.addTarget(self, action: #selector(searchAction), for: .touchUpInside)
 
         NSLayoutConstraint.activate([
-            actionButton.topAnchor.constraint(equalTo: entryPickerView.bottomAnchor, constant: 10),
+            actionButton.topAnchor.constraint(equalTo: entryTextField.bottomAnchor, constant: 30),
             actionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             actionButton.heightAnchor.constraint(equalToConstant: 50),
             actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
@@ -100,6 +128,7 @@ class MainVC: UIViewController {
     }
 
     @objc private func searchAction() {
+        view.endEditing(true)
         navigationController?.pushViewController(ShowingCovDataVC(department: pickerSelection), animated: true)
     }
 }
@@ -122,6 +151,7 @@ extension MainVC: UIPickerViewDelegate, UIPickerViewDataSource {
 
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         pickerSelection = department.code[row].name
+        entryTextField.text = "\(department.code[row].index) - \(department.code[row].name)"
     }
 
 }
